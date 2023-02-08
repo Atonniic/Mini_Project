18bac6d5b4bef1cdc6b74d889adee0d92b6bbbfd
Narawit Sariddeechaikul <narawitpun@gmail.com>
2023-02-08 15:27:45 +0700

    2

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,53 @@ int lou_brightness = 255;
 
 int ldr = 255;
 
+void manual_hw_control(void *param) {
+    while (true) {
+        if (light_kit_on) {
+            ledcWrite(0, light_kit_value);
+        } else {
+            ledcWrite(0, 0);
+        }
+        if (light_bed_on) {
+            ledcWrite(1, light_bed_value);
+        } else {
+            ledcWrite(1, 0);
+        }
+        if (light_lou_on) {
+            ledcWrite(2, light_lou_value);
+        } else {
+            ledcWrite(2, 0);
+        }
+    }
+}
+
+void detect_on_off(void *param) {
+    int kit_last = 50;
+    int kit_current = 50;
+    int bed_last = 50;
+    int bed_current = 50;
+    int lou_last = 50;
+    int lou_current = 50;
+    while (true) {
+        kit_current = touchRead(KITSWITCH);
+        bed_current = touchRead(BEDSWITCH);
+        lou_current = touchRead(LOUSWITCH);
+        if (kit_current - kit_last < -20) {
+            light_kit_on = !light_kit_on;
+        }
+        if (bed_current - bed_last < -20) {
+            light_bed_on = !light_bed_on;
+        }
+        if (lou_current - lou_last < -20) {
+            light_lou_on = !light_lou_on;
+        }
+        kit_last = kit_current;
+        bed_last = bed_current;
+        lou_last = lou_current;
+        vTaskDelay(25/portTICK_PERIOD_MS);
+    }
+}
+
 void bed_control(void *param){
     while(1) {
         if (bed_mode == 0) {
@@ -58,9 +105,16 @@ void Read_LDR(void *param) {
 void setup(){
     Serial.begin(115200);
     Connect_Wifi();
-    //xTaskCreatePinnedToCore(Control, "Control", 10000, NULL, 1, NULL, 0);
-    //xTaskCreatePinnedToCore(Read_LDR, "Read_LDR", 10000, NULL, 1, NULL, 0); 
+    ledcSetup(0, 5000, 8);
+    ledcAttachPin(BED, 0);
+    ledcSetup(1, 5000, 8);
+    ledcAttachPin(KIT, 1);
+    ledcSetup(2, 5000, 8);
+    ledcAttachPin(LOU, 2);
+    xTaskCreatePinnedToCore(Read_LDR, "Read_LDR", 10000, NULL, 1, NULL, 0); 
     xTaskCreatePinnedToCore(HTTP, "HTTP", 10000, NULL, 1, NULL, 1);
+    xTaskCreatePinnedToCore(detect_on_off, "detect_on_off", 20000, NULL, 1, NULL, 1);
+    xTaskCreatePinnedToCore(manual_hw_control, "control", 20000, NULL, 1, NULL, 0);
 }
 
 void loop(){
